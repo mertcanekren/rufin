30ec2abcb00b47246287affd1de54cdd9ee8367c
Mertcan Ekren <mertcanekren@windowslive.com>
2013-07-29 10:01:48 +0300

    create homepage

--- application/views/rufin/header.php
+++ application/views/rufin/header.php
@@ -1,13 +1,34 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
 <head>
 	<title>rufin</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<meta name="viewport" content="width=device-width" />
-	<link rel="shortcut icon" href="/themes/notunburada/img/favicon.ico" type="image/x-icon"/>
-	<link rel="icon" href="/themes/notunburada/img/favicon.ico" type="image/x-icon"/>
-	<link rel="stylesheet" type="text/css" href="/themes/notunburada/css/style.css"/>
-	<script type="text/javascript" src="/themes/notunburada/js/jquery.js"></script>
-	<script type="text/javascript" src="/themes/notunburada/js/javascript.js"></script>
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
+	<link rel="icon" href="favicon.ico" type="image/x-icon"/>
+	<link rel="stylesheet" type="text/css" href="themes/v1/css/css.css"/>
+	<script type="text/javascript" src="themes/v1/js/jquery.js"></script>
 </head>
 <body>
+    <div class="page">
+        <div class="header">
+            <div class="logo">
+                rufin
+            </div>
+            <div class="menu">
+                <ul>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+        <hr>
+        <div class="homepage-lead">
+            <h2>Homepage Title</h2>
+            <p class="lead">Homepage Desc</p>
+            <a class="btn btn-large btn-success" href="login.html">Login</a>
+        </div>
+        <hr>
+        <div class="footer">
+            <p>rufin © 2013 </p>
+        </div>
