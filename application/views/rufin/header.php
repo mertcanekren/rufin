c5b77dd005a2a1400ca2f3b43dab1879117309c3
Mertcan Ekren <mertcanekren@windowslive.com>
2013-07-29 21:40:07 +0300

    adding links

--- application/views/rufin/header.php
+++ application/views/rufin/header.php
@@ -17,7 +17,7 @@
             </div>
             <div class="menu">
                 <ul>
-                    <li><a href="">About</a></li>
+                    <li><a href="about">About</a></li>
                     <li><a href="">Contact</a></li>
                 </ul>
             </div>
--- application/views/rufin/homepage/homepage.php
+++ application/views/rufin/homepage/homepage.php
@@ -1,5 +1,5 @@
 <div class="homepage-lead">
     <h2>Homepage Title</h2>
     <p class="lead">Homepage Desc</p>
-    <a class="btn btn-large btn-success" href="login.html">Login</a>
+    <a class="btn btn-large btn-success" href="login">Login</a>
 </div>
