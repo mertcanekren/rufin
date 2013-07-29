b9523ecf551a484310a9d753c5f028bd10c14b74
Mertcan Ekren <mertcanekren@windowslive.com>
2013-07-29 16:00:53 +0300

    adding login function description

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -10,6 +10,12 @@ class User extends CI_Controller{
         parent::__construct();
     }
 
+   /**
+     * @since 29.07.2013
+     * @author Mertcan EKREN <mertcanekren at windowslive.com>
+     * @template /views/rufin/user/login/login.php
+     * User login
+     */
     public function login(){
         $data["pagetitle"] = "login";
         echo $this->rf_template->View('rufin',$data,array("header"=>"header","footer"=>"footer"),"user/login/login.php");
