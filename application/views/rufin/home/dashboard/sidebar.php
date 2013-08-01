ee3cc698fc6ef786f390fd421817082d78b5ec80
Mertcan Ekren <mertcanekren@windowslive.com>
2013-08-01 15:24:17 +0300

    create sidebar

--- application/views/rufin/home/dashboard/sidebar.php
+++ application/views/rufin/home/dashboard/sidebar.php
@@ -1,3 +1,22 @@
 <div class="sidebar">
-         asds
+    <div class="sidebar-main box">
+        <div class="sidebar-list">
+            <div class="sidebar-list-body"><a href="">asd</a></div>
+        </div>
+        <div class="sidebar-list">
+            <div class="sidebar-list-body"><a href="">asd</a></div>
+        </div>
+        <div class="sidebar-list">
+            <div class="sidebar-list-body"><a href="">asd</a></div>
+        </div>
+        <div class="sidebar-list">
+            <div class="sidebar-list-body"><a href="">asd</a></div>
+        </div>
+        <div class="sidebar-list">
+            <div class="sidebar-list-body"><a href="">asd</a></div>
+        </div>
+        <div class="sidebar-list">
+            <div class="sidebar-list-body"><a href="">asd</a></div>
+        </div>
+    </div>
 </div>
