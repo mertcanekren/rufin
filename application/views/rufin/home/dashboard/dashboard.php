89edd7732e6a2bf84819dea2a4537646347fef9d
Mertcan Ekren <mertcanekren@windowslive.com>
2013-09-08 15:35:09 +0300

    files update

--- application/views/rufin/home/dashboard/dashboard.php
+++ application/views/rufin/home/dashboard/dashboard.php
@@ -1,5 +1,5 @@
 <div class="dashboard">
-    <div class="main">
+    <div class="main">        
         <div class="new-diary box">
             <div class="new-diary-button"><?php echo $new; ?></div>
             <div class="new-diary-form box" id="home-new-diary-form">
@@ -23,6 +23,19 @@
                         </div>
                     </form>
                     <div class="clear"></div>
+                </div>
+            </div>
+        </div>
+        <div class="diary-row">
+            <div class="diary-body">
+                <div class="diary-date">
+                    <div class="diary-date-day">18</div>
+                    <div class="diary-date-month">Temmuz</div>
+                </div>
+                <div class="diary-content">
+                    Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır metinlerdir. Lorem Ipsum, adı bilinmeyen bir matbaacının bir hurufat numune kitabı oluşturmak üzere bir yazı galerisini alarak karıştırdığı 1500'lerden beri endüstri standardı sahte metinler olarak kullanılmıştır. Beşyüz yıl boyunca varlığını sürdürmekle kalmamış, aynı zamanda pek değişmeden elektronik dizgiye de sıçramıştır. 1960'larda Lorem Ipsum pasajları da içeren Letraset yapraklarının yayınlanması ile ve yakın zamanda Aldus PageMaker gibi Lorem Ipsum sürümleri içeren masaüstü yayıncılık yazılımları ile popüler olmuştur.
+
+
                 </div>
             </div>
         </div>
