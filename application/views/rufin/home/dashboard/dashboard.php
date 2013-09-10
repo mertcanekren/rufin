e9784d6175e4d7e3a0f15c03cc06a0453b869c0e
Mertcan Ekren <mertcanekren@windowslive.com>
2013-09-11 00:27:52 +0300

    adding css transition

--- application/views/rufin/home/dashboard/dashboard.php
+++ application/views/rufin/home/dashboard/dashboard.php
@@ -31,11 +31,10 @@
                 <div class="diary-date">
                     <div class="diary-date-day">18</div>
                     <div class="diary-date-month">Temmuz</div>
+                    <div class="diary-date-year">2013</div>
                 </div>
                 <div class="diary-content">
                     Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır metinlerdir. Lorem Ipsum, adı bilinmeyen bir matbaacının bir hurufat numune kitabı oluşturmak üzere bir yazı galerisini alarak karıştırdığı 1500'lerden beri endüstri standardı sahte metinler olarak kullanılmıştır. Beşyüz yıl boyunca varlığını sürdürmekle kalmamış, aynı zamanda pek değişmeden elektronik dizgiye de sıçramıştır. 1960'larda Lorem Ipsum pasajları da içeren Letraset yapraklarının yayınlanması ile ve yakın zamanda Aldus PageMaker gibi Lorem Ipsum sürümleri içeren masaüstü yayıncılık yazılımları ile popüler olmuştur.
-
-
                 </div>
             </div>
         </div>
