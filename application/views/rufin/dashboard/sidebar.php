6ea09b390cceb79d81ff772ba59bdf242ad13be9
Mertcan Ekren <mertcanekren@windowslive.com>
2013-12-18 17:25:15 +0200

    commit

---
+++ application/views/rufin/dashboard/sidebar.php
@@ -0,0 +1,19 @@
+<div class="sidebar">
+    <div class="sidebar-main box">
+        <div class="sidebar-list">
+            <div class="sidebar-list-body"><a href="home">Anasayfa</a></div>
+        </div>
+        <div class="sidebar-list" sub-menu="1">
+            <div class="sidebar-list-body">Projeler</div>
+        </div>
+        <div class="sub-sidebar" id="submenu-1">
+
+            <div class="sub-sidebar-list">
+                <div class="sub-sidebar-list-body"><a href="">Proje Adı</a></div>
+            </div>
+        </div>
+        <div class="sidebar-list">
+            <div class="sidebar-list-body"><a href="">Çıkış</a></div>
+        </div>
+    </div>
+</div>
