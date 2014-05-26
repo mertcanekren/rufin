860ea653d685cbe81eee180dbe4fb4e851c2bd6b
mertcanekren <mertcanekren@windowslive.com>
2014-05-26 23:27:09 +0300

    dashboard sorgu güncellemesi

--- app/controllers/DashboardController.php
+++ app/controllers/DashboardController.php
@@ -3,7 +3,7 @@
 class DashboardController extends BaseController {
 
 	public function dashboard(){
-        $data["issue"] = IssueModel::where('users', '=', Auth::user()->id)->orderBy('id', 'DESC')->get()->take(10)->toArray();
+        $data["issue"] = IssueModel::where('users', '=', Auth::user()->id)->where('status','=','0')->orderBy('id', 'DESC')->get()->take(10)->toArray();
 		return View::make('dashboard.dashboard',compact('data'));
 	}
 
