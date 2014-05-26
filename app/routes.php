8430e782034de8fa7f25b7bae1dfa3372ce23ed7
mertcanekren <mertcanekren@windowslive.com>
2014-05-26 21:03:25 +0300

    route düzenlendi

--- app/routes.php
+++ app/routes.php
@@ -11,33 +11,33 @@
 |
 */
 
-Route::get('/', array('uses' => 'DashboardController@dashboard', 'as' => 'home'));
+Route::get('/', array('uses' => 'DashboardController@dashboard', 'as' => 'home', 'before' => 'auth'));
 Route::get('/dashboard', array('uses' => 'DashboardController@dashboard', 'as' => 'dashboard', 'before' => 'auth'));
 
 /* Talep */
-Route::get('/new-issue', array('uses' => 'IssueController@newIssue', 'as' => 'new-issue'));
-Route::post('/add-issue', array('uses' => 'IssueController@addIssue', 'as' => 'add-issue'));
-Route::get('/issue/{id}', array('uses' => 'IssueController@getIssue', 'as' => 'issue'));
-Route::get('/issue/{id}/edit', array('uses' => 'IssueController@editIssue', 'as' => 'edit-issue'));
+Route::get('/new-issue', array('uses' => 'IssueController@newIssue', 'as' => 'new-issue', 'before' => 'auth'));
+Route::post('/add-issue', array('uses' => 'IssueController@addIssue', 'as' => 'add-issue', 'before' => 'auth'));
+Route::get('/issue/{id}', array('uses' => 'IssueController@getIssue', 'as' => 'issue', 'before' => 'auth'));
+Route::get('/issue/{id}/edit', array('uses' => 'IssueController@editIssue', 'as' => 'edit-issue', 'before' => 'auth'));
 
 /* Talep */
 
 /* Proje */
-Route::get('/new-project', array('uses' => 'ProjectController@newProject', 'as' => 'new-project'));
-Route::post('/add-project', array('uses' => 'ProjectController@addProject', 'as' => 'add-project'));
-Route::get('/project/{id}', array('uses' => 'ProjectController@getProject', 'as' => 'get-project'));
+Route::get('/new-project', array('uses' => 'ProjectController@newProject', 'as' => 'new-project', 'before' => 'auth'));
+Route::post('/add-project', array('uses' => 'ProjectController@addProject', 'as' => 'add-project', 'before' => 'auth'));
+Route::get('/project/{id}', array('uses' => 'ProjectController@getProject', 'as' => 'get-project', 'before' => 'auth'));
 /* Proje */
 
 
 /* Kullanıcı */
 Route::get('/login', array('uses' => 'UserController@login', 'as' => 'login'));
 Route::post('/signin', array('uses' => 'UserController@signIn', 'as' => 'signin'));
-Route::get('/logout', array('uses' => 'UserController@logout', 'as' => 'logout'));
-Route::get('/profile/{id}', array('uses' => 'UserController@profile', 'as' => 'profile'));
+Route::get('/logout', array('uses' => 'UserController@logout', 'as' => 'logout', 'before' => 'auth'));
+Route::get('/profile/{id}', array('uses' => 'UserController@profile', 'as' => 'profile', 'before' => 'auth'));
 
 /* Kullanıcı */
 
 /* Ajax */
-Route::get('/getProjectsList', array('uses' => 'AjaxController@getProjectsList', 'as' => 'get-projectslist'));
+Route::get('/getProjectsList', array('uses' => 'AjaxController@getProjectsList', 'as' => 'get-projectslist', 'before' => 'auth'));
 
 /* Ajax*/
