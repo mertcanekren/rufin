5a90a8de47279e5f0fcd89ff11bdba5143f3dcce
mertcanekren <mertcanekren@windowslive.com>
2014-06-05 20:19:28 +0300

    #6 talep ekranı revize edildi

--- app/views/issue/issue.blade.php
+++ app/views/issue/issue.blade.php
@@ -23,29 +23,38 @@
     @endif
 </div>
 <h3 class="page-header">Detaylar</h3>
-<dl class="dl-horizontal">
-    <dt>{{ Form::label('', Lang::get('project.reporter_user')) }}</dt>
-    <dd><p>{{ HTML::link(URL::route('profile',array('id'  => $data["reporter"]["id"])), $data["reporter"]["username"]) }}</p></dd>
-    <dt>{{ Form::label('', Lang::get('project.assigned_user')) }}</dt>
-    <dd><p>{{ HTML::link(URL::route('profile',array('id'  => $data["users"]["id"])), $data["users"]["username"]) }}</p></dd>
-    <dt>{{ Form::label('', Lang::get('general.created_at')) }}</dt>
-    <dd><p title="{{date('d.m.Y H:i',$data["issue"]["createtime"])}}">{{date('d.m.Y',$data["issue"]["createtime"])}}</p></dd>
-    <dt>{{ Form::label('', Lang::get('general.status')) }}</dt>
-    <dd><p>{{$data["issue"]["status_v"]}}</p></dd>
-    <dt>{{ Form::label('', Lang::get('project.components')) }}</dt>
-    <dd><p>{{$data["issue"]["component_view"]["content"]}}</p></dd>
-    <dt>{{ Form::label('', Lang::get('issue.type')) }}</dt>
-    <dd><p>{{$data["issue"]["type_view"]["content"]}}</p></dd>
-    <dt>{{ Form::label('', Lang::get('project.labels')) }}</dt>
-    <dd><p>
-        @if(isset($data["issue"]["labels_view"]))
-            @foreach ($data["issue"]["labels_view"] as $labels_v)
-            <span class="label label-default">{{$labels_v["content"]}}</span>
-            @endforeach
-        @endif
-        </p>
-    </dd>
-</dl>
+<div class="col-xs-6">
+    <dl class="dl-horizontal">
+        <dt>{{ Form::label('', Lang::get('project.assigned_user')) }}</dt>
+        <dd><p>{{ HTML::link(URL::route('profile',array('id'  => $data["users"]["id"])), $data["users"]["username"]) }}</p></dd>
+        <dt>{{ Form::label('', Lang::get('general.created_at')) }}</dt>
+        <dd><p title="{{date('d.m.Y H:i',$data["issue"]["createtime"])}}">{{date('d.m.Y',$data["issue"]["createtime"])}}</p></dd>
+        <dt>{{ Form::label('', Lang::get('general.status')) }}</dt>
+        <dd><p>{{$data["issue"]["status_v"]}}</p></dd>
+        <dt>{{ Form::label('', Lang::get('project.components')) }}</dt>
+        <dd><p>{{$data["issue"]["component_view"]["content"]}}</p></dd>
+
+    </dl>
+</div>
+<div class="col-xs-6">
+    <dl class="dl-horizontal">
+        <dt>{{ Form::label('', Lang::get('project.reporter_user')) }}</dt>
+        <dd><p>{{ HTML::link(URL::route('profile',array('id'  => $data["reporter"]["id"])), $data["reporter"]["username"]) }}</p></dd>
+
+        <dt>{{ Form::label('', Lang::get('issue.type')) }}</dt>
+        <dd><p>{{$data["issue"]["type_view"]["content"]}}</p></dd>
+        <dt>{{ Form::label('', Lang::get('project.labels')) }}</dt>
+        <dd><p>
+                @if(isset($data["issue"]["labels_view"]))
+                @foreach ($data["issue"]["labels_view"] as $labels_v)
+                <span class="label label-default">{{$labels_v["content"]}}</span>
+                @endforeach
+                @endif
+            </p>
+        </dd>
+    </dl>
+</div>
+<div class="row"></div>
 <h3 class="page-header">{{Lang::get('general.content')}}</h3>
 <p>{{$data["issue"]["content"]}}</p>
 @stop
