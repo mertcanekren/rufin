37da872c87683c6465e02be2898f7909efee01a0
mertcanekren <mertcanekren@windowslive.com>
2014-06-06 23:55:54 +0300

    #8 proje sayfasında tabların yerleri değiştirildi

--- app/views/project/project.blade.php
+++ app/views/project/project.blade.php
@@ -2,11 +2,63 @@
 @section('content')
 <h3>{{ $data["project"]["name"]}}</h3>
 <ul class="nav nav-tabs  nav-tabs-google" id="myTab">
-    <li class="active"><a href="#summary" data-toggle="tab">Özet</a></li>
-    <li><a href="#issue" data-toggle="tab">Talepler</a></li>
+    <li class="active"><a href="#issue" data-toggle="tab">Talepler</a></li>
+    <li><a href="#summary" data-toggle="tab">Özet</a></li>
 </ul>
 <div class="tab-content">
-    <div class="tab-pane active" id="summary">
+    <div class="tab-pane active" id="issue">
+        @if($data["issue"])
+        <div class="col-xs-12 col-sm-12 ">
+            <table class="table" style="margin-top: 10px">
+                <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Talep</th>
+                    <th>Kime Atanmış</th>
+                    <th>Bileşenler</th>
+                    <th>Durumu</th>
+                    <th>İşlemler</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach ($data["issue"] as $issue)
+                <tr>
+                    <td>#{{$issue["id"]}}</td>
+                    <td>{{$issue["title"]}}</td>
+                    <td>Kime Atanmış</td>
+                    <td>
+                        @if(isset($issue["components_view"]))
+                        @foreach ($issue["components_view"] as $components_v)
+                        <span class="label label-default">{{$components_v["content"]}}</span>
+                        @endforeach
+                        @endif
+                    </td>
+                    <td>
+                        @if($issue["status"] == 0)
+                        <span class="label label-default">Açık</span>
+                        @elseif($issue["status"] == 1)
+                        <span class="label label-primary">İşlem Yapılıyor</span>
+                        @else
+                        <span class="label label-success">Tamamlandı</span>
+                        @endif
+                    </td>
+                    <td>
+                        {{ HTML::link(URL::route('issue',array('id' => $issue["id"])), Lang::get('general.viewed'), array('class' => 'btn btn-default btn-xs')) }}
+                        @if(Auth::user()->id == $issue["creator"])
+                        {{ HTML::link(URL::route('edit-issue',array('id' => $issue["id"])), Lang::get('general.edit'), array('class' => 'btn btn-primary btn-xs')) }}
+                        @endif
+
+                    </td>
+                </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+        @else
+        <h5>{{ Lang::get('issue.no_issue') }}</h5>
+        @endif
+    </div>
+    <div class="tab-pane" id="summary">
         <div class="col-xs-12 col-sm-12 ">
             <dl class="dl-horizontal">
                 <dt>Versiyon</dt>
@@ -27,58 +79,6 @@
                 </dd>
             </dl>
         </div>
-    </div>
-    <div class="tab-pane" id="issue">
-        @if($data["issue"])
-        <div class="col-xs-12 col-sm-12 ">
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Talep</th>
-                        <th>Kime Atanmış</th>
-                        <th>Bileşenler</th>
-                        <th>Durumu</th>
-                        <th>İşlemler</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($data["issue"] as $issue)
-                    <tr>
-                        <td>#{{$issue["id"]}}</td>
-                        <td>{{$issue["title"]}}</td>
-                        <td>Kime Atanmış</td>
-                        <td>
-                            @if(isset($issue["components_view"]))
-                                @foreach ($issue["components_view"] as $components_v)
-                                <span class="label label-default">{{$components_v["content"]}}</span>
-                                @endforeach
-                            @endif
-                        </td>
-                        <td>
-                            @if($issue["status"] == 0)
-                            <span class="label label-default">Açık</span>
-                            @elseif($issue["status"] == 1)
-                            <span class="label label-primary">İşlem Yapılıyor</span>
-                            @else
-                            <span class="label label-success">Tamamlandı</span>
-                            @endif
-                        </td>
-                        <td>
-                            {{ HTML::link(URL::route('issue',array('id' => $issue["id"])), Lang::get('general.viewed'), array('class' => 'btn btn-default btn-xs')) }}
-                            @if(Auth::user()->id == $issue["creator"])
-                                {{ HTML::link(URL::route('edit-issue',array('id' => $issue["id"])), Lang::get('general.edit'), array('class' => 'btn btn-primary btn-xs')) }}
-                            @endif
-
-                        </td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
-        @else
-            <h5>{{ Lang::get('issue.no_issue') }}</h5>
-        @endif
     </div>
 </div>
 @stop
